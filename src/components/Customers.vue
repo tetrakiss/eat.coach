5a37501b2ec90c67d3c90792c007d498bdf467d2
Arseny <togulev@gmail.com>
2018-12-03 19:35:06 +0300

    Add tags component

--- src/components/Customers.vue
+++ src/components/Customers.vue
@@ -45,7 +45,8 @@
   <main id="page-wrap">
     <el-card class="box-card" v-for="(customer, index) in customers" :key="`customer-${index}`">
         <div slot="header" class="clearfix">
-
+        
+           <tagsBar v-bind:customerRef="customer['.key']" />
             <span>{{ customer.firstName }} {{ customer.lastName }}</span>
             <span>{{ new Date(customer.dateNextInteraction).toLocaleDateString() }}</span>
             <el-button style="float: right; padding: 5px; margin:5px;" @click="deleteCustomer(customer)" type="danger">Удалить</el-button>
@@ -104,6 +105,7 @@ import {db} from '@/components/firebaseInit'
 import datePicker from '@/components/datePicker'
 import Menu from '@/components/Menu';
 import slide from '@/components/Slide';
+import TagsBar from '@/components/Tags';
 
 export default {
     name: 'customers',
@@ -127,11 +129,20 @@ export default {
     firestore() {
         return {
             customers: db.collection('customers')
+            /*customers: db.collection('customers').then(
+              (snapshot) => {
+                snapshot.docs.forEach((doc) => {
+                  const tags = doc.data()
+                  customers.push(tags)
+                })
+            })*/
         }
     },
     components: {
         'eat-datePicker': datePicker,
-         slide
+         slide,
+         'tagsBar' : TagsBar
+
 
     },
     methods: {
---
+++ src/components/Tags.vue
@@ -0,0 +1,102 @@
+<style scoped>
+
+.el-tag, .el-tag + .el-tag {
+  background-color: rgba(237,77,61,0.8);
+  border: 1px solid rgba(237,77,61,1);
+    margin-left: 10px;
+}
+.el-icon-close:hover {
+  background-color: darkred !important;
+}
+ .el-icon-close {
+  color: darkred !important;
+}
+.el-tag a, .el-tag a:visited, .el-tag a:hover {
+  text-decoration: none;
+  color: white;
+}
+
+
+.button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+}
+
+.input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
+}
+</style>
+<template>
+<div>
+  <el-tag :key="`tag-${index}`" v-for="(tag, index) in dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
+      <a href="#" @click.prevent="searchByTag(tag.tag)">{{tag.tag}}</a>
+  </el-tag>
+  <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="mini" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
+  </el-input>
+  <el-button v-else class="button-new-tag" size="small" @click="showInput">+ Добавить тег</el-button>
+</div>
+
+</template>
+
+<script>
+import {db} from '@/components/firebaseInit'
+    export default {
+      name: 'TagsBar',
+      data() {
+        return {
+          dynamicTags: [],
+          inputVisible: false,
+          inputValue: '',
+        };
+      },
+      props: {
+        customerRef: {
+          type: [String],
+          required: true
+        }
+      },
+      methods: {
+            searchByTag(tag)  {
+              console.log("redirect to search with " + tag)
+              this.$router.push({
+                  name: 'search',
+                  params: {
+                      tag: tag
+                  }
+              });
+            },
+            handleClose(tag) {
+                    db.collection('customers').doc(this.customerRef).collection('tags').doc(tag['.key']).delete();
+                },
+
+                showInput() {
+                    this.inputVisible = true;
+                    this.$nextTick(_ => {
+                        this.$refs.saveTagInput.$refs.input.focus();
+                    });
+                },
+
+                handleInputConfirm() {
+                    let inputValue = this.inputValue;
+                    if (inputValue) {
+                        db.collection('customers').doc(this.customerRef).collection('tags').add({
+                                tag: inputValue
+                            })
+                            //this.dynamicTags.push(inputValue);
+                    }
+                    this.inputVisible = false;
+                    this.inputValue = '';
+                }
+      },
+      firestore() {
+          return {
+              dynamicTags: db.collection('customers').doc(this.customerRef).collection('tags')
+          }
+      }
+    }
+</script>
